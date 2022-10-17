2c3819183d9d65bcdda364261e942f66dee5d905
sandhya-iniyan <sandhya.iniyan@gmail.com>
2022-10-16 19:52:23 -0700

    Create consumer.py

---
+++ CodeLab4/consumer/code/consumer.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import requests
+import json
+import os
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def get_meal():
+   #avoid hard-code dependencies
+   # response = requests.get('http://api:5000/')
+   response = requests.get('http://' + str(os.environ.get("API_HOST")) + ':' + str(os.environ.get("API_PORT")) + "/" + str(os.environ.get("API_ENDPOINT")))
+   response = response.content
+   response = json.loads(response)
+   meal = response['Meal']
+   price = response['Price']
+   
+   return render_template('menu.html', meal = meal, price = price)
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port = 80, debug=True)
