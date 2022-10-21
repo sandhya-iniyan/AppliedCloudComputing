02164eb474c9207cb237b7c218b3eedf3ba79dd8
sandhya-iniyan <sandhya.iniyan@gmail.com>
2022-10-21 17:01:31 -0400

    Update api.py

--- CodeLab5/api/code/api.py
+++ CodeLab5/api/code/api.py
@@ -1,10 +1,31 @@
-FROM python:3.10.6
+from flask import Flask
+import random
+import json
+import os
+#import psycopg2
+app = Flask(__name__)
+import psycopg2
 
-VOLUME /app
-WORKDIR /app
 
-COPY requirements.txt /tmp/
-RUN pip3 install -r /tmp/requirements.txt
+@app.route('/')
+def meal_recommender():
+    conn = psycopg2.connect(database=os.environ.get('POSTGRES_USER'),
+                            user=os.environ.get('POSTGRES_USER'),
+                            password=os.environ.get('POSTGRES_PASSWORD'),
+                            host=os.environ.get('DB_HOST'),
+                            port=os.environ.get('DB_PORT'))
 
-EXPOSE 5000
-CMD [ "python", "api.py" ]
+    #Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+    cursor.execute(
+        '''SELECT Meal, Price FROM Menu ORDER BY random() limit 1;''')
+    #Fetching 1st row from the table
+    result = cursor.fetchall()
+    conn.close()
+    recommendation = {"Meal": result[0][0], "Price": str(result[0][1])}
+    return json.dumps(recommendation)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get("API_PORT"))
+
